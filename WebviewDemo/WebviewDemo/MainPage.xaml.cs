0983bf4ce61b6ea3ff79a5485f863a354c09ba86
caothuong <caothuong0905uit@gmail.com>
2020-08-17 13:17:53 +0700

    Add files via upload

---
+++ WebviewDemo/WebviewDemo/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace WebviewDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public ICommand NavigateCommand { private set; get; }
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            NavigateCommand = new Command<string>(async (string url) =>
+            {
+                await Navigation.PushAsync(new WebPage(url));
+            });
+
+            BindingContext = this;
+        }
+    }
+}
---
+++ WebviewDemo/WebviewDemo/WebPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WebviewDemo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WebPage : ContentPage
+	{
+		public WebPage (string url)
+		{
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            var webView = new WebView
+            {
+                Source = new UrlWebViewSource
+                {
+                    Url = url,
+                },
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            this.Content = new StackLayout
+            {
+                Children = { webView }
+            };
+        }
+	}
+}
